fe61880610ce4108ebfce633000f45ae34e74838
lodge <lodge6410@vip.qq.com>
2019-10-10 14:43:55 +0800

    58done

--- my_mission/Tong58/selenium58_yhzx.py
+++ my_mission/Tong58/selenium58_yhzx.py
@@ -4,26 +4,28 @@
 # @Author  : Lodge
 import os
 import re
+from threading import Thread
+
+import cv2
 import sys
-import time
 import json
+import time
 import base64
 import random
-import urllib3
 import logging
+import urllib3
 import importlib
 from functools import wraps
-from threading import Thread
 from logging import getLogger
-from fontTools.ttLib import TTFont
 import matplotlib.pyplot as plt
-from helper.Baidu_ocr.client import main_ocr
+from fontTools.ttLib import TTFont
+from skimage.measure import compare_ssim
+# from helper.Baidu_ocr.client import main_ocr
 
 import requests
 from lxml import etree
 from selenium import webdriver
 from fake_useragent import UserAgent
-# from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions   # 不用EC 因为EC会有小黄下划线 不好看
@@ -74,13 +76,41 @@ class TongCheng(object):
         print(info)
 
     @staticmethod
-    def handle_num(key):
+    def down_compare_img():
+        image_dynamic = BASE_DIR + r"\helper\fonts\plt.png"   # 这就是动态的图片,通过前面程序保存的
+
+        print('开始比较图片中...')
+        for i in range(10):  # 意思就是识别图片0 到 9
+            time.sleep(0.1)
+            image1 = BASE_DIR + rf"\helper\fonts\digit_{i}.png"     # 待识别的字符图片
+            image_a = cv2.imread(image1)
+            image_b = cv2.imread(image_dynamic)
+            gray_a = cv2.cvtColor(image_a, cv2.COLOR_BGR2GRAY)
+            gray_b = cv2.cvtColor(image_b, cv2.COLOR_BGR2GRAY)
+            (score, diff) = compare_ssim(gray_a, gray_b, full=True)
+            print(f'当前秒为{time.localtime().tm_sec}:图片比较中...和{i}的相似度为{score:.3f}...')
+
+            if score > 0.86:    # 应该这个 魔法 数字比较好用
+                return str(i)   # 返回的就是那个数字
+            else:
+                continue
+        return None
+
+    def handle_num(self, key, flag=None):
         key_code = re.findall(r'(&#x.*?);', key)
-        if len(key_code) == 7 or 11:
-            key_1 = key_code[0]   # 1
-            key = key.replace(f'{key_1};', '1')
-        else:
+        if flag:
+            print('是年龄...')
+            key_1 = None
             key = key
+        else:
+            if len(key_code) == 11:
+                print('是手机号....')
+                key_1 = key_code[0]   # 1
+                key = key.replace(f'{key_1};', '1')
+            else:
+                print('是薪资....')
+                key_1 = key_code[-1]  # 0
+                key = key.replace(f'{key_1};', '0')
 
         xml = etree.parse(BASE_DIR + r'\helper\fonts\b64.xml')
         root = xml.getroot()
@@ -97,16 +127,21 @@ class TongCheng(object):
             font_dict[font_key] = json.dumps(contour_list, sort_keys=True)
 
         for each_value in key_code:
-            if each_value == key_code[0]:
+            if each_value == key_1:
+                print('跳过了指定字符为:0/1:')
                 continue
             else:
+                print('当前处理剩下的数据...')
                 each_value = each_value.replace('&#x', '')
+                print('当前需要处理的数据为:', each_value)
                 for v in font_dict:
-                    value = font_dict[v]
-                    value = json.loads(value)
-                    x = []
-                    y = []
+                    # print('当前的', v, '有跑到这里...')
                     if v == each_value:
+                        value = font_dict[v]
+                        value = json.loads(value)
+                        x = []
+                        y = []
+                        time.sleep(0.1)
                         for al in value:
                             x_e = int(al['x'])
                             y_e = int(al['y'])
@@ -118,16 +153,16 @@ class TongCheng(object):
                         plt.plot(x, y)  # 这里可以额外添加很多属性比如线型,线色('-k')这个表示实线黑色  线宽(linewidth)
                         plt.axis('off')  # 关闭坐标
                         plt.savefig(BASE_DIR + r"\helper\fonts\plt.png")
-                        plt.show()   # 这个和下面那个功能会重置坐标 如果两个都要显示的话 不要放上句前面
+                        # plt.show()   # 这个和上面那个功能会重置坐标 如果两个都要显示的话 不要放上句前面
 
-                        sub_key = main_ocr()   # 调用百度的识别，发现很慢
+                        print('准备比较相似程度...')
+                        sub_key = self.down_compare_img()
+                        # sub_key = main_ocr()   # 调用百度的识别，发现很慢
 
                         if sub_key:
                             key = key.replace(f'&#x{each_value};', sub_key)
                         else:
                             key = key.replace(f'&#x{each_value};', '*')
-                    time.sleep(0.5)
-
         return key
 
     @staticmethod
@@ -146,7 +181,6 @@ class TongCheng(object):
         data_s = handled_data
         for _ in range(order_list):
             data = data_s[_]
-            # print('data:', data)
             gender_judge = data['sex']
             work_exp = data['experiences']
             if work_exp:
@@ -162,10 +196,13 @@ class TongCheng(object):
                 work_experience = []
             phone_num = self.handle_num(data['mobile'])
             name = self.handle_name(data)
-            # work_year = self.handle_num(data['workYear'])
-            work_year = data['workYear']
-            # salary = self.handle_num(data['targetSalary']) if '面议' not in data['targetSalary'] else '面议'
-            salary = data['targetSalary'] if '面议' not in data['targetSalary'] else '面议'
+            work_year = self.handle_num(data['workYear'])
+            salary = self.handle_num(data['targetSalary']) if '面议' not in data['targetSalary'] else '面议'
+
+            age = self.handle_num(data['ageText'], True)
+            print('年龄为:', age)
+            now_year = time.localtime().tm_year
+            age = now_year
 
             json_info = {
                 'name': name,
@@ -173,7 +210,7 @@ class TongCheng(object):
                 'company_dpt': 1,   # 不确定写啥
                 'resume_key': data['expectCateName'],
                 'gender': 2 if gender_judge == '0' else 1,
-                'date_of_birth': f'-01-01',
+                'date_of_birth': f'{age}-01-01',
                 'current_residency': data['expectArea'],
                 'years_of_working': work_year,
                 'hukou': '',
@@ -205,25 +242,22 @@ class TongCheng(object):
                 'certifications': [],
                 'is_viewed': 1,
                 'resume_date': '',
-                'get_type': '',
+                'get_type': 2,
                 'external_resume_id': data['resumeid'][-49:],
                 'labeltype': 1,
                 'resume_logo': data['picUrl'],
-                'resume_from': 3,           # 这里忘记该写啥了
+                'resume_from': 4,           # 来源
                 'account_from': python_config.account_from,
                 'update_date': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
             }
 
             yield json_info
 
-    def click_get_code(self):
+    def click_get_code(self, num):
         time.sleep(0.5)
-        all_code_pos = self.driver.find_elements_by_xpath(
-            '/html/body/div[2]/div[2]/div[1]/div[4]/div[2]/ul/li/div[1]/div[3]/section[1]')
-        num_all_code = len(all_code_pos)
-        if num_all_code != 0:
-            time.sleep(0.3)
-            for i in range(1, num_all_code+1):
+        if num != 0:
+            time.sleep(0.2)
+            for i in range(1, num+1):
                 each_pos = self.driver.find_element_by_xpath(
                     f'/html/body/div[2]/div[2]/div[1]/div[4]/div[2]/ul/li[{i}]/div[1]/div[3]/section[1]'
                 )
@@ -233,7 +267,7 @@ class TongCheng(object):
                         f'/html/body/div[2]/div[2]/div[1]/div[4]/div[2]/ul/li[{i}]/div[1]/div[3]/section[1]/a'
                     )
                     each_pos_c.click()
-                    time.sleep(1)
+                    time.sleep(0.3)
 
                 if i == 5:
                     break
@@ -261,9 +295,8 @@ class TongCheng(object):
         pgs = people_num // 50 + 1
         for pg in range(1, pgs+1):
             LOG.info(f'当前下载页为:{pg}, 页面数据有{people_num}条')
-            time.sleep(random.uniform(1, 3))
+            time.sleep(random.uniform(0, 1))
             now_pg_data = requests.get(self.down_url_api, params={'pageindex': pg}, headers=headers, verify=False)
-            # print('now_pg_data:', now_pg_data.text)
             new_data = now_pg_data.text.lstrip('(').rstrip(')')
             new_data = json.loads(new_data)
             resume_data = new_data['data']['resumeList']
@@ -274,15 +307,21 @@ class TongCheng(object):
         # /html/body/div[2]/div[2]/div[1]/div[4]/div[2]/ul/li/div[1]/div[1]/div[3]/p[1]/span[1]
         now_page_referer = self.driver.current_url
         pages = today_all_num   # 总共的数量(后面两个调用函数意思不一样)
-        time.sleep(random.uniform(1, 3))
+        time.sleep(random.uniform(0, 1))
         # 下面是好一点的办法,速度快,但是需要处理的地方有点多
         resume_data = self.download_page_all_detail(now_page_referer, pages)  # 这里的pages是该函数拿来确定页数的
+        tttt1 = time.time()
         for each_data in resume_data:
             data_info_down = self.transfer_useful(each_data, pages)   # 这里的pages是用来保留几条数据的
             for post_data_down in data_info_down:
                 self.post_data_down(post_data_down)
 
-    def handle_font(self, page_source):
+        tttt2 = time.time()
+
+        print('总用时:', tttt2 - tttt1)
+
+    @staticmethod
+    def handle_font(page_source):
         bs64_str = re.findall(
             r'@font-face{font-family:"customfont"; src:url\(data:application/font-woff;charset=utf-8;base64,(.*?)\)  format',
             page_source)[0]
@@ -293,30 +332,13 @@ class TongCheng(object):
         font = TTFont(BASE_DIR + '/helper/fonts/b64.woff')
         font.saveXML(BASE_DIR + '/helper/fonts/b64.xml')
 
-        get_cmap = font['cmap'].getBestCmap()
-        new_map = dict()
-
-        for key in get_cmap.keys():
-            value = re.search(r'(\d+)', get_cmap[key])
-            if not value:
-                continue
-            # print(get_cmap[key])
-            value = int(re.search(r'(\d+)', get_cmap[key]).group(1)) - 1
-            key = hex(key)
-            new_map[key] = value
-
-        pass
-
     def download_page_people(self):
         # 2.这里先判断今天有没有人
         today_tik = time.strftime('%Y-%m-%d', time.localtime())
-        today_tik = '2018-11-26'
+        today_tik = '2019-07-06'
         LOG.info(f'今天日期为{today_tik},下载任务开始查询...')
         page_source = self.driver.page_source
         self.handle_font(page_source)
-        # print('开始休息了...')           # # # #
-        # time.sleep(10000)
-        # self.click_get_code()    # 这里是处理页面的数据，让每一个都变成可以识别的密令
         try:
             today_all = self.driver.find_elements_by_xpath(
                 f'/html/body/div[2]/div[2]/div[1]/div[4]/div[2]/ul/li/div[1]/div[text()="{today_tik}"]')
@@ -325,6 +347,8 @@ class TongCheng(object):
             today_all_num = 0
         else:
             today_all_num = len(today_all)
+
+        self.click_get_code(today_all_num)  # 这里是处理页面的数据，让每一个都变成可以识别的密令
 
         if today_all_num != 0:
             self.download_page_people_each(today_all_num)
@@ -367,7 +391,6 @@ class TongCheng(object):
         self.driver.get(self.auto_resume_url)
         today_tic = time.strftime('%Y-%m-%d', time.localtime())
         xpath_today = f'投递时间：{today_tic}'
-        xpath_today = '投递时间：2019-10-06'
         time.sleep(0.5)
         all_auto = self.driver.find_elements_by_xpath(f'//span[text()="{xpath_today}"]')
         all_nums_auto = len(all_auto)
@@ -445,21 +468,24 @@ class TongCheng(object):
                 'certifications': [],
                 'is_viewed': 1,
                 'resume_date': '',
-                'get_type': '',
+                'get_type': 1,
                 'external_resume_id': data['resumeid'][-49:],
                 'labeltype': 1,
                 'resume_logo': data['picUrl'],
-                'resume_from': 3,  # 这里忘记该写啥了
+                'resume_from': 4,  #
                 'account_from': python_config.account_from,
                 'update_date': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
             }
 
             yield json_info
 
-    def run(self):
+    # ================================================================ #
+
+    def run_down(self):
         self.download_page()    # 获取下载的简历信息 (涉及到加密,正在处理中)
 
-        # self.auto_resume()   # 获取主动投递的简历信息
+    def run_auto(self):
+        self.auto_resume()   # 获取主动投递的简历信息
 
 
 def send_rtx_msg(msg):
@@ -491,20 +517,39 @@ def clear_timer(set_time):
                 print()
                 func(*args, **kwargs)
                 print('\r这个程序停了,正在休眠中...', end='')
-                # print('清除程序开始了,正在休眠中...')
                 time.sleep(60)
             else:
                 print('\r这个程序停了,继续休眠中...', end='')
-                # print('不是清除程序运行的时间,继续休眠中...')
                 time.sleep(60)
 
         return wrapper
     return work_time
 
 
-def main():
+# @clear_timer(['21:00'])
+def main_down():
     app = TongCheng()
-    app.run()
+    app.run_down()
+
+
+@clear_timer(['09:00', '11:30', '16:00', '18:30', '23:55'])
+def main_auto():
+    app = TongCheng()
+    app.run_auto()
+
+
+def main():
+    main_down()
+    print('调试的时候卡死在这里')
+    time.sleep(10000)
+    while True:
+        t_down = Thread(target=main_down)
+        t_auto = Thread(target=main_auto)
+
+        t_down.start()
+        t_auto.start()
+        t_down.join()
+        t_auto.join()
 
 
 if __name__ == '__main__':
